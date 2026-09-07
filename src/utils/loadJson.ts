5441556315e51ae05f5ebe5ea65dcffca0268632
frogstat <frogstatftw@gmail.com>
2026-09-07 11:13:44 +0200

    Give clearer error message during game data load

--- src/utils/loadJson.ts
+++ src/utils/loadJson.ts
@@ -2,12 +2,12 @@ export async function loadJson<T>(jsonPath: string):Promise<T> {
     const response = await fetch(jsonPath);
 
     if (!response.ok) {
-        throw new Error("Could not load json");
+        throw new Error("Could not load " + jsonPath);
     }
     const contentType:string = response.headers.get("content-type") ?? "";
 
     if (!contentType.includes("application/json")) {
-        throw new Error("Request did not return json");
+        throw new Error(jsonPath + " did not return json. Does it exist?");
     }
 
     return await response.json();
